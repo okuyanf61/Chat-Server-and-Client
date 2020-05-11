81e0e57f4f0cda2ae4e5f8c1345c993840a1d05c
Mehmet Fatih Okuyan <okuyanf6161@gmail.com>
2020-05-12 02:47:59 +0300

    Chat room activated

--- client.py
+++ client.py
@@ -1,22 +1,43 @@
 import socket
+import select
+import errno
+import sys
 
 headerSize = 8
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname()
 hostIP = socket.gethostbyname(host)
-s.connect((socket.gethostname(), 561))
+server.connect((socket.gethostname(), 561))
+server.setblocking(False)
+userName = input("What's your username? : ")
+userName = userName.encode("utf-8")
+userNameHeader = f"{len(userName):<{headerSize}}".encode('utf-8')
+server.send(userNameHeader + userName)
+
 while True:
-    fullMessage = ""
-    newMessage = True
-    while True:
-        message = s.recv(32)
-        if newMessage:
-            messageLen = int(message[:headerSize])
-            newMessage = False
+    message = input(f'{userName.decode("utf-8")}: ')
+    if message:
+        message = message.encode("utf-8")
+        messageHeader = f"{len(message):<{headerSize}}".encode("utf-8")
+        server.send(messageHeader + message)
 
-        fullMessage += message.decode("utf-8")
-
-        if len(fullMessage) - headerSize == messageLen:
-            print(fullMessage[headerSize:])
-            newMessage = True
-            fullMessage = ""
+    try:
+        while True:
+            userNameHeader = server.recv(headerSize)
+            if not len(userNameHeader):
+                print("Disconnected from server")
+                sys.exit()
+            userNameLen = int(userNameHeader.decode("utf-8").strip())
+            userName = server.recv(userNameLen).decode("utf-8")
+            messageHeader = server.recv(headerSize)
+            messageLen = int(messageHeader.decode("utf-8").strip())
+            message = server.recv(messageLen).decode("utf-8")
+            print(f"{userName}: {message}")
+    except IOError as error:
+        if error.errno != errno.EAGAIN and error.errno != errno.EWOULDBLOCK:
+            print(f"Reading error: {str(error)}")
+            sys.exit()
+        continue
+    except Exception as error:
+        print(f"Reading error: {str(error)}")
+        sys.exit()
--- server.py
+++ server.py
@@ -1,27 +1,55 @@
 import socket
-import time
+import select
 
 headerSize = 8
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 host = socket.gethostname()
 hostIP = socket.gethostbyname(host)
-s.bind((hostIP, 561))
-s.listen(5)
-print(host)
-print(hostIP)
+hostPort = 561
+server.bind((hostIP, hostPort))
+server.listen(5)
+clientList = [server]
+clients = {}
+print("Machine: " + host)
+print("Server is ready on: " + hostIP + ":" + str(hostPort))
+
+
+def receive_message(client):
+    try:
+        receive_header = client.recv(headerSize)
+
+        if not len(receive_header):
+            return False
+        message_len = int(receive_header.decode("utf-8").strip())
+        return {'header': receive_header, 'data': client.recv(message_len)}
+    except socket.error:
+        return False
+
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    message = "Connection established!"
-    message = f"{len(message):<{headerSize}}" + message
-    clientsocket.send(bytes(message, "utf-8"))
-
-    while True:
-        time.sleep(3)
-        message = f"Current time: {time.time()}"
-        message = f"{len(message):<{headerSize}}" + message
-
-        print(message)
-
-        clientsocket.send(bytes(message, "utf-8"))
+    readClients, writeClients, exceptionClients = select.select(clientList, [], clientList)
+    for notifiedSocket in readClients:
+        if notifiedSocket == server:
+            clientSocket, clientAddress = server.accept()
+            user = receive_message(clientSocket)
+            if user is False:
+                continue
+            clientList.append(clientSocket)
+            clients[clientSocket] = user
+            print('New connection from {}:{}, username: {}'.format(*clientAddress, user['data'].decode('utf-8')))
+        else:
+            message = receive_message(notifiedSocket)
+            if message is False:
+                print('{} disconnected'.format(clients[notifiedSocket]['data'].decode('utf-8')))
+                clientList.remove(notifiedSocket)
+                del clients[notifiedSocket]
+                continue
+            user = clients[notifiedSocket]
+            print(f'{user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+            for clientSocket in clients:
+                if clientSocket != notifiedSocket:
+                    clientSocket.send(user['header'] + user['data'] + message['header'] + message['data'])
+    for notifiedSocket in exceptionClients:
+        clientList.remove(notifiedSocket)
+        del clients[notifiedSocket]
